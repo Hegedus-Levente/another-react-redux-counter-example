2c59214add86046bfcfc31580eafe30f62ae19f0
Hegedus Levente <hayler04@gmail.com>
2017-03-24 12:06:23 +0200

    step2

--- app/container/App.js
+++ app/container/App.js
@@ -3,10 +3,26 @@ import React, { Component } from 'react';
 import Welcome from 'components/Welcome';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      name: 'default'
+    };
+  }
+
+  onKeyUp (event) {
+    this.setState({
+      name: event.target.value
+    });
+  };
+
   render() {
     return (
       <div>
-        <Welcome name='hardcoded' />
+        <Welcome name={this.state.name} />
+        <label>Enter name</label>
+        <input type='text' onKeyUp={this.onKeyUp.bind(this)}/>
       </div>
     );
   }
